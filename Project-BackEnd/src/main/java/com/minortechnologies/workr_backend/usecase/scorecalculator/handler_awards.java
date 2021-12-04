2217726ebd6be0099e38bea9b65f489230dfb940
Domenica Vega <domenica.vega@mail.utoronto.ca>
2021-12-04 00:57:09 -0500

    Making changes to score_calculate() in handler_awards to compute score properly

--- Project-BackEnd/src/main/java/com/minortechnologies/workr_backend/usecase/scorecalculator/handler_awards.java
+++ Project-BackEnd/src/main/java/com/minortechnologies/workr_backend/usecase/scorecalculator/handler_awards.java
@@ -4,6 +4,7 @@ import com.minortechnologies.workr_backend.entities.listing.JobListing;
 import com.minortechnologies.workr_backend.entities.user.User;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class handler_awards extends handler {
 
@@ -21,20 +22,32 @@ public class handler_awards extends handler {
             for(String award: user_awards){
                 int counter = 0;
                 String[] award_words = award.split(" ");
-                System.out.println(award_words);
+                //System.out.println(award_words);
+                String[] words_avoiding = {"about", "above", "across", "after", "against", "along", "among", "around",
+                "at", "because", "before", "behind", "below", "beneath", "beside", "besides", "between", "beyond",
+                "but", "by", "concerning", "despite", "down", "during", "except", "excepting", "for", "from", "in",
+                        "inside", "instead", "into", "like", "near", "of", "off", "on", "onto", "out", "outside",
+                "over", "past", "regarding", "since", "through", "throughout", "to", "toward", "under", "underneath",
+                "until", "up", "upon", "with", "within", "without", "regard"};
+                ArrayList<String> words_to_avoid = new ArrayList<String>(Arrays.asList(words_avoiding));
                 for(String word: award_words){
-                    if (!this.job.getData(JobListing.QUALIFICATIONS).equals("") &&
-                            this.job.getData(JobListing.QUALIFICATIONS).toString().toLowerCase().contains(word.toLowerCase()))
-                        counter += 1;
-                    else if (!this.job.getData(JobListing.REQUIREMENTS).equals("") &&
-                            this.job.getData(JobListing.REQUIREMENTS).toString().toLowerCase().contains(word.toLowerCase()))
-                        counter += 1;
-                    else if (!this.job.getData(JobListing.APPLICATION_REQUIREMENTS).equals("") &&
-                            this.job.getData(JobListing.APPLICATION_REQUIREMENTS).toString().toLowerCase().contains(word.toLowerCase()))
-                        counter += 1;
-                    else if (!this.job.getData(JobListing.DESCRIPTION).equals("") &&
-                            this.job.getData(JobListing.DESCRIPTION).toString().toLowerCase().contains(word.toLowerCase()))
-                        counter += 1;
+                    if (words_to_avoid.contains(word.toLowerCase())){
+                        counter += 0;
+                    } else{
+                        if (!this.job.getData(JobListing.QUALIFICATIONS).equals("") &&
+                                this.job.getData(JobListing.QUALIFICATIONS).toString().toLowerCase().contains(word.toLowerCase()))
+                            counter += 1;
+                        else if (!this.job.getData(JobListing.REQUIREMENTS).equals("") &&
+                                this.job.getData(JobListing.REQUIREMENTS).toString().toLowerCase().contains(word.toLowerCase()))
+                            counter += 1;
+                        else if (!this.job.getData(JobListing.APPLICATION_REQUIREMENTS).equals("") &&
+                                this.job.getData(JobListing.APPLICATION_REQUIREMENTS).toString().toLowerCase().contains(word.toLowerCase()))
+                            counter += 1;
+                        else if (!this.job.getData(JobListing.DESCRIPTION).equals("") &&
+                                this.job.getData(JobListing.DESCRIPTION).toString().toLowerCase().contains(word.toLowerCase()))
+                            counter += 1;
+                    }
+
                 }
                 score += counter * 3;
             }
