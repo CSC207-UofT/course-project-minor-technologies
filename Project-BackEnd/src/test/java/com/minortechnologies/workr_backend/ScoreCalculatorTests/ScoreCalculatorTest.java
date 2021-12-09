b12bad09d41adcaed34b185529a3868377ba4a95
Domenica Vega <domenica.vega@mail.utoronto.ca>
2021-12-08 22:15:30 -0500

    Made changes so it follows Java naming convention

--- Project-BackEnd/src/test/java/com/minortechnologies/workr_backend/ScoreCalculatorTests/ScoreCalculatorTest.java
+++ Project-BackEnd/src/test/java/com/minortechnologies/workr_backend/ScoreCalculatorTests/ScoreCalculatorTest.java
@@ -4,10 +4,11 @@ import com.minortechnologies.workr_backend.entities.listing.CustomJobListing;
 import com.minortechnologies.workr_backend.entities.user.User;
 import com.minortechnologies.workr_backend.usecase.scorecalculator.*;
 
-import com.minortechnologies.workr_backend.usecase.scorecalculator.handler_1;
-import com.minortechnologies.workr_backend.usecase.scorecalculator.handler_2;
-import com.minortechnologies.workr_backend.usecase.scorecalculator.handler_3;
-import com.minortechnologies.workr_backend.usecase.scorecalculator.handler_main;
+import com.minortechnologies.workr_backend.usecase.scorecalculator.HandlerSkills;
+import com.minortechnologies.workr_backend.usecase.scorecalculator.HandlerRelatedWork;
+import com.minortechnologies.workr_backend.usecase.scorecalculator.HandlerUnrelatedWork;
+import com.minortechnologies.workr_backend.usecase.scorecalculator.HandlerMain;
+
 import org.junit.*;
 import static org.junit.Assert.*;
 
@@ -24,10 +25,10 @@ public class ScoreCalculatorTest {
     }
 
     @Test
-    /* Tests that score_calculate() in handler_main.java computes user's score properly.
+    /* Tests that score_calculate() in HandlerMain.java computes user's score properly.
      * */
     public void testHandlerMainScoreCalculate(){
-        handler_main handler = new handler_main(user1,jobl1);
+        HandlerMain handler = new HandlerMain(user1,jobl1);
         handler.generateScore();
         double test_score = handler.getScore();
 
@@ -35,72 +36,77 @@ public class ScoreCalculatorTest {
     }
 
     @Test
-    /* Tests that score_calculate() in handler_1.java computes user's score properly.
+    /* Tests that score_calculate() in HandlerSkills.java computes user's score properly.
      *
      */
-    public void testHandler1ScoreCalculate() {
-        handler_main handler_main1 = new handler_main(user1, jobl1);
-        handler_1 handler1 = new handler_1(user1, jobl1);
+    public void testHandlerSkillsScoreCalculate() {
+        HandlerSkills handler1 = new HandlerSkills(user1, jobl1);
 
         handler1.scoreCalculate();
         assertEquals(handler1.getScore(), 40.0,0.01);
     }
 
     @Test
-    /* Tests that score_calculate() in handler_2.java computes user's score properly.
+    /* Tests that score_calculate() in HandlerRelatedWork.java computes user's score properly.
      *
      */
-    public void testHandler2ScoreCalculate() {
-        handler_2 handler2 = new handler_2(user1, jobl1);
+    public void testHandlerRelatedWorkScoreCalculate() {
+        HandlerRelatedWork handler2 = new HandlerRelatedWork(user1, jobl1);
 
         handler2.scoreCalculate();
         assertEquals(handler2.getScore(), 72.0, 0.01f);
     }
 
     @Test
-    /* Tests that score_calculate() in handler_3.java computes user's score properly.
+    /* Tests that score_calculate() in HandlerUnrelatedWork.java computes user's score properly.
      *
      */
-    public void testHandler3ScoreCalculate() {
-        handler_3 handler3 = new handler_3(user1, jobl1);
+    public void testHandlerUnrelatedWorkScoreCalculate() {
+        HandlerUnrelatedWork handler3 = new HandlerUnrelatedWork(user1, jobl1);
 
         handler3.scoreCalculate();
         assertEquals(handler3.getScore(), 26.0, 0.01f);
     }
 
     @Test
-    /* Tests that score_calculate() in handler_leadership.java computes user's score properly.
+    /* Tests that score_calculate() in HandlerLeadership.java computes user's score properly.
      *
      */
     public void testHandlerLeadershipScoreCalculate() {
-        handler_leadership handlerleadership = new handler_leadership(user1, jobl1);
+        HandlerLeadership handlerleadership = new HandlerLeadership(user1, jobl1);
 
         handlerleadership.scoreCalculate();
         assertEquals(handlerleadership.getScore(), 30.0, 0.01f);
     }
 
     @Test
-    /* Tests that score_calculate() in handler_incentive.java computes user's score properly.
+    /* Tests that score_calculate() in HandlerIncentive.java computes user's score properly.
      *
      */
     public void testHandlerIncentiveScoreCalculate() {
-        handler_incentive handlerincentive = new handler_incentive(user1, jobl1);
+        HandlerIncentive handlerincentive = new HandlerIncentive(user1, jobl1);
 
         handlerincentive.scoreCalculate();
         assertEquals(handlerincentive.getScore(), 15.0, 0.01f);
     }
 
     @Test
+    /* Tests that score_calculate() in HandlerAwards.java computes user's score properly.
+     *
+     */
     public void testHandlerAwardsScoreCalculate() {
-        handler_awards handlerawards = new handler_awards(user1, jobl1);
+        HandlerAwards handlerawards = new HandlerAwards(user1, jobl1);
 
         handlerawards.scoreCalculate();
         assertEquals(handlerawards.getScore(), 9.0, 0.01f);
     }
 
     @Test
-    public void testHandlerGpaScoreCalculate() {
-        handler_6 handler_gpa= new handler_6(user1, jobl1);
+    /* Tests that score_calculate() in HandlerGrade.java computes user's score properly.
+     *
+     */
+    public void testHandlerGradeScoreCalculate() {
+        HandlerGrade handler_gpa= new HandlerGrade(user1, jobl1);
 
         handler_gpa.scoreCalculate();
         assertEquals(handler_gpa.getScore(), 20.0, 0.01f);
