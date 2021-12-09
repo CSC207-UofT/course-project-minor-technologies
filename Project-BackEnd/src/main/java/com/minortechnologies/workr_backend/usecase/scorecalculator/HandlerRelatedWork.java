86fcd071357ecc5a8eb9b56d18311a3ababefad4
raghavbanka <raghavbanka30@gmail.com>
2021-12-09 05:40:29 -0500

    renamed "springadventures" to "networkhandler"

--- Project-BackEnd/src/main/java/com/minortechnologies/workr_backend/usecase/scorecalculator/HandlerRelatedWork.java
+++ Project-BackEnd/src/main/java/com/minortechnologies/workr_backend/usecase/scorecalculator/HandlerRelatedWork.java
@@ -26,13 +26,9 @@ public class HandlerRelatedWork extends HandlerExtWork{
         ArrayList<Experience> user_experiences = (ArrayList<Experience>) this.user.getData(User.REL_WORK_EXP);
         if(user_experiences != null){
             for(Experience experience : user_experiences) {
-                LocalDate start_date = (LocalDate) experience.getData(Experience.START_TIME);
-                LocalDate end_date = (LocalDate) experience.getData(Experience.END_TIME);
-                String title = (String) experience.getData(Experience.EXPERIENCE_TITLE);
                 ArrayList<String> description = (ArrayList<String>) experience.getData(Experience.EXPERIENCE_DESCRPTION);
-                double experience_score = scoreTime(start_date, end_date);
-                score += experience_score * description.size();
-                score += titleScore(title);
+                score += scoreTime(experience) * description.size();
+                score += titleScore(experience);
             }
         }
 
